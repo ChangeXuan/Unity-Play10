9a816a647f049529ca0b1307a9fff94e15f86453
YourName <YourEmailAddress>
2018-02-10 17:21:01 +0800

    multiplayerGame

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+	public const int maxHealth = 100;
+	[SyncVar(hook="onChangeHealth")]
+	public int currentHealth = maxHealth;
+	public Slider healthSlider;
+
+	public void takeDamage(int damage) {
+
+		// 血量的处理只在服务器端执行
+		if (!isServer) {
+			return;
+		}
+
+		currentHealth -= damage;
+		if (currentHealth <= 0) {
+			currentHealth = 0;
+			Debug.Log ("Dead");
+		}
+	}
+
+	void onChangeHealth(int newHealth) {
+		healthSlider.value = newHealth /(float) maxHealth;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+	public GameObject bulletPrefab;
+	public Transform bulletPos;
+
+	void Update () {
+
+		// 判断是否是本地客户端操作		
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Cmdfire ();
+		}
+
+		moveController ();
+	}
+
+	// 重写父类方法
+	// 这个方法只会在本地角色那里调用
+	public override void OnStartLocalPlayer () {
+		GetComponent<MeshRenderer> ().material.color = Color.blue;
+	}
+
+	// 移动旋转控制
+	void moveController() {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		transform.Rotate (Vector3.up * h * 300 * Time.deltaTime);
+		transform.Translate (Vector3.forward * v * 3 * Time.deltaTime);
+	}
+
+	// 子弹的生成应该在server端完成，然后把子弹同步到各个client
+	[Command]//运行在server，响应在client
+	void Cmdfire() {
+		GameObject bullte = Instantiate (bulletPrefab, bulletPos.position, bulletPos.rotation);
+		bullte.GetComponent<Rigidbody> ().velocity = bullte.transform.forward * 10f;
+		Destroy (bullte, 2);
+
+		NetworkServer.Spawn (bullte);
+	}
+}
